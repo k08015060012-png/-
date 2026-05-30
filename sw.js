d46c40f881c0b12b8a67796199b1ae73542a99fe
k08015060012-png <k08015060012@gmail.com>
2026-05-30 15:59:46 +0900

    Initial commit

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'zaiko-v1';
+const ASSETS = [
+  './在庫検索アプリ.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // GASへのAPIリクエストはキャッシュしない（常に最新データを取得）
+  if (e.request.url.includes('script.google.com')) return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
